fbf5b0f79597531d5b51e5494f05009fa6abbca3
Samuel <samucarcoverde@gmail.com>
2024-10-03 17:30:33 -0300

    add serializers and views

--- api_rest/views.py
+++ api_rest/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+from django.http import HttpReponse, JsonResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Veiculo
+from .serializers import VeiculoSerializer
+
+import json
+
+
+
+@api_view(['GET'])
+def get_users(request):
+    
+    if request.method == 'GET':
+        veiculos = Veiculo.objects.all()
+
+        serializer = VeiculoSerializer(veiculos, many=True)
+        return Response(serializer.data)
+    
+    return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def databaseEmDjango():
+
+#    data = Veiculo.objects.get()
+#    data = Veiculo.objects.filter()
+#    data = Veiculo.objects.exclude()
+
+#    data.save()
+
+#    data.delete()
